06be11eceb26bdf6b54a2ff7d979817a2f64368e
FADHIL <104739070+fahmiaziz98@users.noreply.github.com>
2022-07-24 03:53:07 +0700

    Add files via upload

---
+++ fakeplants.py
@@ -0,0 +1,38 @@
+import scrapy
+import pandas as pd
+
+
+list_name = []
+list_cat = []
+
+class QuotesSpider(scrapy.Spider):
+    name = "fakeplants"
+    start_urls = ['https://www.fake-plants.co.uk/']
+
+    # Follow links
+    def parse(self, response):
+        for item in response.css('li.product-category a::attr(href)'):
+            yield response.follow(item.get(), callback=self.parse_categories)
+
+    def parse_categories(self, response):
+        products = response.css('div.astra-shop-summary-wrap')
+        for product in products:
+            name = product.css('h2.woocommerce-loop-product__title::text').get()
+            Categories = product.css('span.ast-woo-product-category::text').get().strip()
+            
+            list_name.append(name)
+            list_cat.append(Categories)
+
+            # print(f'Nama : {list_name} and Categories : {list_cat}')
+        df = pd.DataFrame({
+            'Categories': list_cat,
+            'Name': list_name
+        })
+        df.to_excel('datas.xlsx', index=False)
+
+            # writing data excel
+            # df = pd.DataFrame([items], columns=['Name', 'Categories'])
+            # df.to_excel('dataFakeplants.xlsx', sheet_name='Fake_Data', index=False)
+
+
+        
